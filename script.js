6ad72dc3643d3078c3273345785ec8dc7b171d7f
TheFred94 <frederikrlarsen@gmail.com>
2023-02-06 20:00:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+const Btn = document.querySelector("#outputfield");
+
+Btn.addEventListener("click", generateOutput());
+
+// Function that generates the output from the list of different options
+function generateOutput() {}

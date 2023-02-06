c3ce3f86ab23a0594bc9db5485728e02074d040e
TheFred94 <frederikrlarsen@gmail.com>
2023-02-06 21:49:46 +0100

    second options works first name out of full name

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ function showOutput() {
   let input = document.getElementById("inputfield").value;
   let output = (document.getElementById("outputfield").value = input);
   // Variables for 1st option (0)
-  let firstLet = input.substring(0, 1);
-  let restName = input.substring(1);
-  let capFirstLet = firstLet.toUpperCase();
   console.log(options);
   if (options === "0") {
+    let firstLet = input.substring(0, 1);
+    let restName = input.substring(1);
+    let capFirstLet = firstLet.toUpperCase();
     output = capFirstLet + restName;
+  } else if (options === "1") {
+    const fullName = input;
+    let firstName = "";
+    firstName = fullName.substring(0, fullName.indexOf(" "));
+    output = firstName;
+    console.log(firstName);
+  } else if (options === "2") {
+  } else if (options === "3") {
+  } else if (options === "4") {
+  } else if (options === "5") {
+  } else if (options === "6") {
+  } else if (options === "7") {
   }
   document.getElementById("outputfield").value = output;
   console.log(output);

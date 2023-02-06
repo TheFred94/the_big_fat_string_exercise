84fd853c0c187f9399c90186de33ddefd98f967c
TheFred94 <frederikrlarsen@gmail.com>
2023-02-06 21:32:08 +0100

    if statement and operator works

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-"use strict";
-const Btn = document.querySelector("#outputfield");
+"use strict;";
 
-Btn.addEventListener("click", generateOutput());
+const generateOutput = document.querySelector("#generateoutput").addEventListener("click", showOutput);
 
-// Function that generates the output from the list of different options
-function generateOutput() {}
+function showOutput() {
+  // variable for the the options
+  let options = document.getElementById("options").value;
+  //   Gets the value from the input field
+  let input = document.getElementById("inputfield").value;
+  let output = (document.getElementById("outputfield").value = input);
+  // Variables for 1st option (0)
+  let firstLet = input.substring(0, 1);
+  let restName = input.substring(1);
+  let capFirstLet = firstLet.toUpperCase();
+  console.log(options);
+  if (options === "0") {
+    output = capFirstLet + restName;
+  }
+  document.getElementById("outputfield").value = output;
+  console.log(output);
+}
